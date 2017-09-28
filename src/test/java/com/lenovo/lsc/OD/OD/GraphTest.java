1acc6b057e1d0784a639fe0e53deeb77413c4c85
zhaodj5 <zhaodj5@lenovo.com>
2017-09-28 18:06:32 +0800

    [zhaodj5]aa

--- src/test/java/com/lenovo/lsc/OD/OD/GraphTest.java
+++ src/test/java/com/lenovo/lsc/OD/OD/GraphTest.java
@@ -16,55 +16,59 @@ import java.util.stream.Collector;
 public class GraphTest {
 
     @Test
-    public void test() {
-
-        //load data
-        ImmutableMultimap.Builder<String, String> builder = ImmutableMultimap.builder();
-        builder.put("VT1:1", "MEM:4G");
-        builder.put("VT1:1", "CPU:I5");
-        builder.put("VT1:1", "CPU:I7");
-        builder.put("VT2:1", "CPU:I5");
-        builder.put("VT2:1", "HDD:256G");
-        builder.put("VT2:2", "CPU:I7");
-        builder.put("VT2:2", "HDD:128G");
-        builder.put("VT2:2", "HDD:256G");
-        builder.put("VT3:1", "MEM:4G");
-        builder.put("VT3:1", "CPU:I5");
-        builder.put("VT3:1", "Card:NV810");
+    public void testOD() {
 
 
         //covert graph
         MutableValueGraph<String, String> graph = ValueGraphBuilder.undirected().build();
 
-        ImmutableMultimap<String, String> ods = builder.build();
 
-        ImmutableSet<String> key_ods = ods.keySet();
-        for (String key : key_ods) {
+        //load data
+        ImmutableMultimap.Builder<String, String> variantBuilder = ImmutableMultimap.builder();
+        variantBuilder.put("VT1:1", "MEM:4G");
+        variantBuilder.put("VT1:1", "CPU:I5");
+        variantBuilder.put("VT1:1", "CPU:I7");
+        variantBuilder.put("VT2:1", "CPU:I5");
+        variantBuilder.put("VT2:1", "HDD:256G");
+        variantBuilder.put("VT2:2", "CPU:I7");
+        variantBuilder.put("VT2:2", "HDD:128G");
+        variantBuilder.put("VT2:2", "HDD:256G");
+        variantBuilder.put("VT3:1", "MEM:4G");
+        variantBuilder.put("VT3:1", "CPU:I5");
+        variantBuilder.put("VT3:1", "Card:NV810");
+
+
+
+
+        ImmutableMultimap<String, String> variantMultmap = variantBuilder.build();
+
+        ImmutableSet<String> variantMultmapkey = variantMultmap.keySet();
+        for (String key : variantMultmapkey) {
 
             System.out.println("key=>>" + key);
 
             String t = key.split(":")[0];
             String s = key.split(":")[1];
 
-            System.out.println("value=>" + ods.get(key));
+            System.out.println("value=>" + variantMultmap.get(key));
 
-            ImmutableCollection<String> odsKey = ods.get(key);
-            Multimap<String, String> cvMultimap = odsKey.stream().collect(Collector.of(ArrayListMultimap::create, (multimap, vt) -> {
+            ImmutableCollection<String> variantValues = variantMultmap.get(key);
+            Multimap<String, String> variantValueMultimap = variantValues.stream().collect(Collector.of(ArrayListMultimap::create, (multimap, vt) -> {
                 multimap.put(vt.split(":")[0], vt.split(":")[1]);
             }, (multi1, multi2) -> {
                 multi1.putAll(multi2);
                 return multi1;
             }));
 
-            System.out.println("cvMultimap => " + cvMultimap);
+            System.out.println("cvMultimap => " + variantValueMultimap);
 
-            for (int i = 0; i < cvMultimap.keySet().size() - 1; i++) {
+            for (int i = 0; i < variantValueMultimap.keySet().size() - 1; i++) {
 
-                for (int j = i + 1; j < cvMultimap.keySet().size(); j++) {
-                    String charI = (String) cvMultimap.keySet().toArray()[i];
-                    Collection<String> valueI = cvMultimap.get(charI);
-                    String charJ = (String) cvMultimap.keySet().toArray()[j];
-                    Collection<String> valueJ = cvMultimap.get(charJ);
+                for (int j = i + 1; j < variantValueMultimap.keySet().size(); j++) {
+                    String charI = (String) variantValueMultimap.keySet().toArray()[i];
+                    Collection<String> valueI = variantValueMultimap.get(charI);
+                    String charJ = (String) variantValueMultimap.keySet().toArray()[j];
+                    Collection<String> valueJ = variantValueMultimap.get(charJ);
 
                     valueI.forEach(vI -> {
                                 valueJ.forEach(vJ -> {
@@ -81,16 +85,6 @@ public class GraphTest {
 
         System.out.println("graph => " + graph);
 
-//        graph.adjacentNodes("MEM:4G").forEach(
-//                cv -> {
-//                    System.out.println(cv);
-//                    System.out.println(graph.edgeValue("MEM:4G",cv));
-//
-//                }
-//
-//        );
-
-//        Traversal traversal = new Traversal("EPCOUNTRY:MALAYSIA", graph);
         Traversal traversal = new Traversal("MEM:4G", graph);
         System.out.println(traversal.traversal());
 
